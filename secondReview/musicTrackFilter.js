b2055e7fa00d95859a13eece024f50001a24df27
Oana <oana.demian@gmail.com>
2023-03-30 15:37:24 +0100

    returns 40 for an array containing one sound of 40 Hz frequency

--- secondReview/musicTrackFilter.js
+++ secondReview/musicTrackFilter.js
@@ -1,7 +1,9 @@
 soundwavesFilter = (soundwaves) => {
   if (soundwaves.length === 0) {
   return 'no frequencies have been supplied';
-  };
+  } else {
+    return soundwaves;
+  }
 } 
 
 module.exports = soundwavesFilter;
--- secondReview/musicTrackFilter.test.js
+++ secondReview/musicTrackFilter.test.js
@@ -1,14 +1,21 @@
 // band pass filter 
-// input: soundwaves: an array of frequences, each frequency is an integer
+// input: soundwaves: an array of frequencies, each frequency is an integer
 // output: soundwaves: an array of the same length
 // default upper limit:1000 
 // default lower limit:40
 // empty array: 'no frequencies have been supplied'
+// check if values are integers
+//next: practice on smth fun, like building a game in the console
 
 const soundwavesFilter = require ('./musicTrackFilter');
 describe ('band pass filter ', () => {
   it ('returns message no frequencies have been supplied when the input is an empty array', () => {
     const soundwaves = [];
     expect(soundwavesFilter(soundwaves)).toEqual('no frequencies have been supplied');
+  });
+
+  it ('returns 40 for an array containing one sound of 40 Hz frequency', () => {
+    const soundwaves = [40];
+    expect(soundwavesFilter(soundwaves)).toEqual([40]);
   });
 });
